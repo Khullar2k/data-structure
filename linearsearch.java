64a285d5d76bdf7793acf590f505e18e3d49ea00
Khullar2k <68366559+Khullar2k@users.noreply.github.com>
2020-07-23 11:10:23 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -3,25 +3,37 @@ class linearsearch
 {
  public static void main(String[] args)
  {  Scanner sc =new Scanner (System.in);
-     int n = sc.nextInt();
+     
+  int n = sc.nextInt();
      int a[]=new int[n];
      for(int i=0;i<n;i++)
      {
+      
        a[i]=sc.nextInt();
      }
+  
      int x=sc.nextInt();
-     int j;
-     int flag=0;
+     int j,flag=0;
      for(j = 0; j < n; j++) 
     { 
+      
+      
         if(a[j] == x) 
           { 
-            flag=1;
-        break;}
+            
+         flag=1;
+        break;
+        }
     } 
+  
+  
     if(flag==1)
-    System.out.print("found at"+" "+j);
-    else
-    System.out.print("not found");
+    {
+    System.out.print("f at"+" "+j);
+     
+    }
+    else{
+     
+    System.out.print("n f");}
  }
 }
