665f77d3c66b7711cf3797ca0890e452a42704a0
Khullar2k <68366559+Khullar2k@users.noreply.github.com>
2020-07-23 11:00:16 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -3,33 +3,49 @@ import java.util.Scanner;
 public class Bubblesort {
     public static void main(String args[])
     {
-        int temp;
+        int t;
+        
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the size of an array");
+        System.out.println("size of array=");
         int n = sc.nextInt();
+        
         int a[]=new int [n];
-        System.out.println("enter the elements");
+        System.out.println("elements=");
         for(int i=0;i<n;i++)
+            
+            
         {
             a[i]=sc.nextInt();
         }
+        
+        
         for(int i=0;i<n-1;i++)
         {
+            
+            
             int flag=0;
             for(int j=0;j<n-1-i;j++)
+                
             {
-                if(a[j]>a[j+1])
+                
+              if(a[j]>a[j+1])
                 {
-                 temp=a[j];
+                  
+                 t=a[j];
                  a[j]=a[j+1];
-                 a[j+1]=temp;
+                 a[j+1]=t;
+                  
                 }
+                
                 flag=1;
 
             }
+            
             if(flag==0)
                 break;
         }
-        for(int i=0;i<a.length;i++)
+             
+   for(int i=0;i<a.length;i++)
+       
         System.out.println(a[i]);
     }
